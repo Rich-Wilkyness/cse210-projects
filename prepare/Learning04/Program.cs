268a15e6ddaba30f381edc8d20a7e549ee861483
UnbrokenBond <r.christian.wilkerson@gmail.com>
2023-05-08 22:59:47 -0600

    finsihed prepare activity Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Jerry", "Math");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Mike", "Division", "7.3", "8-19");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mary", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
